2d0ddeabe96eb799a1e652cdded5cdbd4e970538
Takaki Miyajima <takaki.mz@gmail.com>
2022-07-25 21:04:21 -0700

    added main compoent implemantation

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
-
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="mainContainer">
+      <div className="cryptContainer">
+        <h1 className="title">Crypt Card</h1>
+        <button type="button">
+          <p className="buttonText">Connect wallet</p>
+        </button>
+      </div>
+
+      <div className="inputContainer">
+        <input type="text" placeholder="Address" name="address" />
+        <input type="number" placeholder="Currency (ETH)" name="amount" step={"0.0001"} />
+        <button type="button">Send</button>
+      </div>
+    </div>
+  );
+};
